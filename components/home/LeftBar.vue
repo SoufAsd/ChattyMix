8974160d8b006029b21407b18779aa9c03bf64b5
SoufAsd <assaadi669@gmail.com>
2023-02-21 16:56:11 +0100

    index layout

---
+++ components/home/LeftBar.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div class="side">
+        <NGrid :cols="4" layout-shift-disabled>
+          <NGi>
+            <div class="servers"></div>
+          </NGi>
+          <NGi :span="3">
+            <div class="sections"></div>
+            <div class="audio"></div>
+          </NGi>
+        </NGrid>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss">
+@import "@/assets/sass/pages/left-side.sass";
+</style>
